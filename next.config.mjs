fefbcea8e2e849ec40f67f396deab4bd6ebb6f38
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-02-05 13:54:57 +0600

    added in next.config.js file restrict mode: true and also added serverActions:true

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+const nextConfig = {
+    reactStrictMode: true,
+    experimental: {
+      serverActions: true, // Enable Server Actions
+    },
+  };
+  
+  export default nextConfig;
