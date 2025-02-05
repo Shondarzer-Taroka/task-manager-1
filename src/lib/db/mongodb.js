c72ebe390d51873d6a2f5bbe208222e4f7563656
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-02-05 22:06:25 +0600

    view part finished

--- src/lib/db/mongodb.js
+++ src/lib/db/mongodb.js
@@ -1,22 +1,24 @@
 
 
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config()
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
 export async function connectDB() {
   if (mongoose.connection.readyState >= 1) {
-    return; // ✅ Prevent multiple connections
+    return; 
   }
 
   try {
     await mongoose.connect(MONGODB_URI, {
-      dbName: "task-manager", // ✅ Add your database name
+      dbName: "task-manager", 
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("❌ MongoDB Connection Error:", error);
+    console.error("MongoDB Connection Error:", error);
   }
 }
